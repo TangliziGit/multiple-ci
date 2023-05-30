4308fa0cea1daf1cdb4759c3626ff497354647c7
TangliziGit <tanglizimail@foxmail.com>
2023-05-30 11:18:04 +0800

    chore: rename scanner cli command parameters

--- lib/multiple_ci/cli/scanner.py
+++ lib/multiple_ci/cli/scanner.py
@@ -15,19 +15,19 @@ from multiple_ci.config import config
 @click.option('--redis-db', default=config.DEFAULT_REDIS_DB, help='redis db to store metas')
 @click.option('--mq-host', default=config.DEFAULT_MQ_HOST, help='AMQP message queue host')
 @click.option('--upstream-url', default=config.DEFAULT_UPSTREAM_URL, help='the upstream repo url')
-@click.option('--debug', default=None, help='send a repo without checker just for debug')
-@click.option('--submit', default=None)
+@click.option('--plan', default=None, help='submit a plan without scanner checker')
+@click.option('--job', default=None)
 @click.argument('commands', nargs=-1)
-def main(redis_host, redis_port, redis_db, mq_host, upstream_url, debug, submit, commands):
+def main(redis_host, redis_port, redis_db, mq_host, upstream_url, plan, job, commands):
     LOG_FORMAT = "%(asctime)s [%(levelname)s]: %(message)s"
-    logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
+    logging.basicConfig(level=logging.ERROR, format=LOG_FORMAT)
     caches.CacheManager.init(redis_host, redis_port, redis_db)
 
-    if debug is not None:
+    if plan is not None:
         fifo = os.path.join(config.DEFAULT_MULTIPLE_CI_HOME, 'notify.pipe')
         notify = { 'file': [ fifo ] }
-        scanner.Scanner(mq_host, upstream_url=upstream_url).send(debug, notify)
-    elif submit is not None:
+        scanner.Scanner(mq_host, upstream_url=upstream_url).send(plan, notify)
+    elif job is not None:
         # TODO: arguments for scanner host and port
         req.post('localhost:8081/job', data={
             'commands': commands
