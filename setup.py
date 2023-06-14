24f930d56b4bda49820f99dd6f072375ba4691eb
TangliziGit <tanglizimail@foxmail.com>
2023-06-14 09:46:24 +0800

    chore: restore setup.py and services

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='multiple-ci',
+    version='0.0.1',
+    install_requires=['pika', 'PyYAML', 'redis', 'Click', 'tornado', 'elasticsearch>=8.6', 'requests',
+                      'readerwriterlock'],
+    packages=find_packages('lib'),
+    package_dir={'': 'lib'},
+    entry_points={
+        'console_scripts': [
+            'mci-scanner=multiple_ci.cli.scanner:main',
+            'mci-scheduler=multiple_ci.cli.scheduler:main',
+            'mci-analyzer=multiple_ci.cli.analyzer:main',
+            'mci-notifier=multiple_ci.cli.notifier:main',
+        ],
+    },
+)
